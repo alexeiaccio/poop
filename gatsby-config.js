141fe250e4b3088af7722f38b691e08c4f63510e
Alexei Accio <alexei.accio@gmail.com>
2018-09-17 20:36:55 +0300

    new round

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: "Poop"
-  },
-  plugins: ["gatsby-plugin-react-helmet"]
-};
--- src/components/Img.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default ({ src, children }) => (
-  <div className="relative">
-    <img src={src} alt="Крапивный суп" width="100%" />
-    <p className="absolute">{children}</p>
-  </div>
-);
