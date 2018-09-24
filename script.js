a1fb693b8d3bda48d609a2a2a9e02f6a7f47fafa
Alexei Accio <alexei.accio@gmail.com>
2018-09-24 21:34:23 +0300

    default

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ if (window !== undefined) {
   window.addEventListener("touchmove", e => moveHandler(e));
 }
 
+const defaultYuha = 0;
+
 const Smile = document.getElementById("smile");
 const Yuha = document.getElementById("yuha");
 
 const opacityHandler = x => y => (x.style.opacity = y);
+
+opacityHandler(Yuha)(defaultYuha);
 
 const moveHandler = e => {
   const y = (e.touches ? e.touches[0].screenY : e.offsetY) / window.innerHeight;
