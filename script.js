4205a97d0ff92cf1adb5e03d00a9d8613acd61d2
Alexei Accio <alexei.accio@gmail.com>
2018-09-24 21:29:42 +0300

    yuha invert

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ const opacityHandler = x => y => (x.style.opacity = y);
 const moveHandler = e => {
   const y = (e.touches ? e.touches[0].screenY : e.offsetY) / window.innerHeight;
   opacityHandler(Smile)(y);
-  opacityHandler(Yuha)(y);
+  opacityHandler(Yuha)((1 / y) * 0.1);
 };
